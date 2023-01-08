e5e6e79433df3c9eecdb6187fe04701a2556eb88
BloodyDracula <39646409+BloodyDracula@users.noreply.github.com>
2023-01-08 14:29:53 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ValeronS/massage-aksay.git main:gh-pages
+git push -f git@github.com:BloodyDracula/bloodydracula.github.io.git main:gh-pages
 
 cd -
